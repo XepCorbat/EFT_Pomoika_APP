a54de74d47c4384528ba80ea5bc06a413377f8aa
XepCorbat <roman11rr@mail.ru>
2023-08-31 20:11:15 +0300

    Add support RU

--- EFT_pomoika.py
+++ EFT_pomoika.py
@@ -6,8 +6,9 @@ from tkinter import ttk
 def run_query(aue=None):
     query = '''
     {
-      itemsByName(name: "''' + entry.get() + '''") {
+      items(lang: ru, name: "''' + entry.get() + '''") {
         avg24hPrice
+        name
         sellFor {
           price,
           vendor{name}
@@ -18,13 +19,14 @@ def run_query(aue=None):
     headers = {"Content-Type": "application/json"}
     response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
     if response.status_code == 200:
-        kot = "o predmete\n"
-        for i in response.json()['data']['itemsByName']:
-            kot+=f'\navg: {i["avg24hPrice"]}\nsellFor:\n'
+        subject_values = "o predmete\n"
+        print(response.json())
+        for i in response.json()['data']['items']:
+            subject_values+=f'\n{i["name"]}:\n Средняя цена на барахолке -> {i["avg24hPrice"]}\n'
             for j in i["sellFor"]:
-                kot+=f"   цена: {j['price']} vender:{j['vendor']['name']}\n"
-            kot+="--------------------"
-        label['text']=kot
+                subject_values+=f" {j['vendor']['name']} -> {j['price']} \n"
+            subject_values+="--------------------"
+        label['text'] = subject_values
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
 
