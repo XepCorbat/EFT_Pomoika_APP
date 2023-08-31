c9d7e71e5a3516f3b453e8a7589a8e25caf3b490
Rafaboba <boss.leonidov@mail.ru>
2023-08-31 16:57:55 +0300

    рома иди нахуй

--- EFT_pomoika.py
+++ EFT_pomoika.py
@@ -3,7 +3,7 @@ from tkinter import*
 from tkinter import ttk
 
 
-def run_query():
+def run_query(aue=None):
     query = '''
     {
       itemsByName(name: "''' + entry.get() + '''") {
@@ -18,7 +18,13 @@ def run_query():
     headers = {"Content-Type": "application/json"}
     response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
     if response.status_code == 200:
-        label["text"]=str(response.json())
+        kot = "o predmete\n"
+        for i in response.json()['data']['itemsByName']:
+            kot+=f'\navg: {i["avg24hPrice"]}\nsellFor:\n'
+            for j in i["sellFor"]:
+                kot+=f"   цена: {j['price']} vender:{j['vendor']['name']}\n"
+            kot+="--------------------"
+        label['text']=kot
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
 
@@ -30,7 +36,8 @@ entry = ttk.Entry()
 entry.pack(anchor=NW, padx=6, pady=6)
 btn = ttk.Button(text="iwiMraz'", command=run_query)
 btn.pack(anchor=NW, padx=6, pady=6)
-
+entry.bind('<Return>',run_query)
 label = ttk.Label()
 label.pack(anchor=NW, padx=6, pady=6)
+
 root.mainloop()
