e43ea14e3763a2fb4d5be230dd266929e0a00e37
Rafaboba <boss.leonidov@mail.ru>
2023-08-31 16:29:33 +0300

    norm kod2.0

--- EFT_pomoika.py
+++ EFT_pomoika.py
@@ -1,23 +1,36 @@
 import requests
-itemName=input()
-def run_query(query):
+from tkinter import*
+from tkinter import ttk
+
+
+def run_query():
+    query = '''
+    {
+      itemsByName(name: "''' + entry.get() + '''") {
+        avg24hPrice
+        sellFor {
+          price,
+          vendor{name}
+        }
+      }
+    }
+    '''
     headers = {"Content-Type": "application/json"}
     response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
     if response.status_code == 200:
-        return response.json()
+        label["text"]=str(response.json())
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
 
-new_query = '''
-{
-  itemsByName(name: "'''+itemName+'''") {
-    avg24hPrice
-    sellFor {
-      price,
-      vendor{name}
-    }
-  }
-}
-'''
-result = run_query(new_query)
-print(result)
+
+root=Tk()
+root.title('Romapidor')
+root.geometry('300x600')
+entry = ttk.Entry()
+entry.pack(anchor=NW, padx=6, pady=6)
+btn = ttk.Button(text="iwiMraz'", command=run_query)
+btn.pack(anchor=NW, padx=6, pady=6)
+
+label = ttk.Label()
+label.pack(anchor=NW, padx=6, pady=6)
+root.mainloop()
